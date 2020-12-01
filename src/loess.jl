588a0eaf648f8f9048ee8767db355a11cf312a13
Fran Urbano <viraltux@gmail.com>
2020-12-01 09:26:34 +0100

    faster implementation without broadcasting

--- src/loess.jl
+++ src/loess.jl
@@ -1,84 +1,47 @@
 ## Loess
 using LinearAlgebra
 using Statistics
-## TODO: consider returning an Array instead a DataFrame since Arrays
-## are default types
 
-# Least Square Fitting adjusted by Weights and Variance Factors
-function lsq(x,y;
-             d=2,
-             w = repeat([1.0],inner=length(x)),
-             k = repeat([1.0],inner=length(x)))
+## TODO: Though much faster than before still much slower than Loess.loess
+##       Things to look:
+##       1- faster lsq?
+##       2- KDTree? - simplify lambda for unidimensional series
+##       3- pass A instead xv,xy
 
-    @assert (d==1) | (d==2)
-    @assert all(w.>=0) & all(k.>0) "weights must be positive or zero and variance factors positive"
-
-    ## Removing zero weighted values from to speed up calculations
-    x = x[w.!=0]
-    y = y[w.!=0]
-    k = k[w.!=0]
-    w = w[w.!=0]
-    
-    # Ax = b
-    A = hcat(x,repeat([1],inner=length(x)))
-    b = y
-    d == 2 ? A = hcat(x .^ 2, A) : nothing
-    
-    ## Considering errors are uncorrelated to simplify the calculations with weigths 
-    A = A .* (w .* (1 ./ k))
-    b = b .* (w .* (1 ./ k))
-
-    # Removing pinv since it fails for long series > 10,000 and introduces
-    # artifacts (jumps) in the smoothing.
-    # Using pinv to avoid near singular errors with recommended tolerance
-    # rtol=sqrt(eps(real(float(one(eltype(A))))))
-    # pinv(A'*A,rtol=rtol)*A'*b
-    (A'*A)\(A'*b)
-end
-
-# using DataFrames
-# lm(@formula(y~x),DataFrame(x=x,y=y))
-
-function weights(u::Float64;type::String="tricube")::Float64
-    @assert type in ["tricube"]
-    @assert u >= 0 "values must be positive or zero"
-    if type == "tricube"
-        # Tricube weight function as described by Cleveland et al.
-        w = (1-u^3)^3
-        return max(0,w)
-    end
-end
-
-# distance of qth farthest xi from x
-function lambda(x::Float64,q::Int64;xv::Array{Float64})
-    @assert (sort(xv) == xv)
-    n = length(xv)
-    q = min(q,n)
-    xvx = abs.(xv .- x)
-    qidx = sortperm(xvx)[1:q]
-    qdist = abs(xv[last(qidx)]-x)*max(1,q/n)
-    (qdist = qdist, qidx = qidx)
-end
-
-# Neighborhood weight for any xi 
-function upsilon(x::Float64,i::Int64;q::Int64,xv::Array{Float64})
-    lambda_qx, qidx = lambda(x,q;xv=xv)
-    i in qidx ? weights(abs(xv[i]-x)/lambda_qx) : 0.0
-end
+## TODO: for d=1 it fails and greatly deviates from Loess.loess
 
 function ghat(x::Float64;xv,yv,
-              k=repeat([1],inner=length(xv)),
+              ik=repeat([1.0],inner=length(xv)),
               q,d=2)
 
+    #lambda q
     n = length(xv)
-    w = upsilon.(x,1:n,q=q,xv=xv)
-    #(@isdefined k) ? nothing :
-    lsq_x = lsq(xv,yv;d=d,w=w,k=k)
-    d == 1 ? [x,1]'*lsq_x : [x^2,x,1]'*lsq_x
+    q = min(q,n)
+    xvx = @. abs(xv-x)
+    qidx = sortperm(xvx)[1:q]
+    qdist = abs(xv[last(qidx)]-x)*max(1,q/n)
+
+    w = zeros(n)
+    #upsilon
+    for wi in 1:n
+        aq = abs(xv[wi]-x)/qdist
+        w[wi] = max((1-aq^3)^3,0)
+    end
+
+    # Ax = b
+    A = hcat(xv,repeat([1.0],inner=length(xv)))
+    b = yv
+    d == 2 ? A = hcat(xv .^ 2.0, A) : nothing
+    
+    ## Considering errors are uncorrelated to simplify the calculations with weigths 
+    A = @. A*(w*ik)
+    b = @. b*(w*ik)
+
+    lsq_x = (A'*A)\(A'*b)
+    
+    d == 1 ? [x,1.0]'*lsq_x : [x^2.0,x,1.0]'*lsq_x
 
 end
-
-using NearestNeighbors
 
 function loess(xv,yv;
                d=2,
@@ -94,5 +57,11 @@ function loess(xv,yv;
     xv = xv[myi]
     yv = yv[myi]
 
-    ghat.(xv;xv=xv,yv=yv,q=q,d=d)
+    #ghat.(xv;xv=xv,yv=yv,q=q,d=d)
+    res = zeros(length(xv))
+    ik = 1.0 ./ k
+    for (i,xi) in enumerate(xv)
+        res[i] = ghat(xi;xv=xv,yv=yv,ik=ik,q=q,d=d)
+    end
+    res
 end
