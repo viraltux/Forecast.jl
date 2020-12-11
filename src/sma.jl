34013845eda54c305ca9b4928466650ebae91dc8
Fran Urbano <viraltux@gmail.com>
2020-12-11 18:18:51 +0100

    flags extend, normx and normy

--- src/sma.jl
+++ src/sma.jl
@@ -1,5 +1,4 @@
-function sma(x, n; extended = false)
-    #TODO implement extended flag for doing ma as long as x
+function sma(x, n; extend = false, normx = false, normy = false)
 
     if n == 1
         return x
@@ -14,26 +13,34 @@ function sma(x, n; extended = false)
     fvp = findlast(!ismissing, x)
     
     # using missing values in 1:a to center ma 
-    a = div(n-1,2)
+    a = normx ? 0 : div(n,2)
 
     # initial moving average value
     ma = sum(x[ivp:n+ivp])/n
-    res[a+ivp+1] = ma
-    for i in 2:(N-n+1-ivp-(N-fvp))
-        resai = ma + (x[n+i-1+ivp] - x[i-1+ivp])/n
+    res[a+ivp] = ma
+    for i in 1:(N-n-ivp-(N-fvp))
+        resai = ma + (x[n+ivp+i] - x[ivp+i])/n
         # missing values are imputed
-        res[a+i+ivp] = ismissing(resai) ? ma : resai
-        ma = res[a+i+ivp]
+        res[a+ivp+i] = ismissing(resai) ? ma : resai
+        ma = res[a+ivp+i]
     end
-
-    if extended
-        b = n-a-1
+    
+    if extend
+        b = n-a
+        #TODO optimize by using just the initial and final values
         aux = hcat(res,x)
         aux = mapslices(x->coalesce(x...),aux,dims=2)[:,1]
         sma2 = collect(skipmissing(sma(aux,n)))
-        res[1:a+ivp]=sma2[1:a+ivp]
+        res[1:a+ivp-1]=sma2[1:a+ivp-1]
         N2 = length(sma2)
         res[(N-b+1):N]=sma2[(N2-b+1):N2]
+    end
+
+    if normy
+        Nm = N-count(x->ismissing(x),x)
+        Nmr = N-count(x->ismissing(x),res)
+        d = sum(skipmissing(x))/Nm-sum(skipmissing(res))/Nmr
+        res = res .+ d
     end
 
     res
