f7e840451531f3094ba4620f2a93483e0f370b02
Fran Urbano <viraltux@gmail.com>
2020-12-12 23:12:47 +0100

    new extend draft

--- src/sma.jl
+++ src/sma.jl
@@ -1,4 +1,4 @@
-function sma(x, n; normx = false, normy = false)
+function sma(x, n; center = false, extend = false, norm = false)
 
     if n == 1
         return x
@@ -13,7 +13,7 @@ function sma(x, n; normx = false, normy = false)
     fvp = findlast(!ismissing, x)
     
     # using missing values to center ma 
-    a = normx ? 0 : div(n,2)
+    a = center ? div(n,2) : 0
 
     # initial moving average value
     ma = sum(x[ivp:n+ivp])/n
@@ -24,15 +24,34 @@ function sma(x, n; normx = false, normy = false)
         res[a+ivp+i] = ismissing(resai) ? ma : resai
         ma = res[a+ivp+i]
     end
-    
-    if normy
-        Nm = N-count(x->ismissing(x),x)
-        Nmr = N-count(x->ismissing(x),res)
-        d = sum(skipmissing(x))/Nm-sum(skipmissing(res))/Nmr
-        res = res .+ d
+
+    if extend
+        # extending the x axis
+        norm01(xv) = (xv .- minimum(xv)) / maximum( (xv .- minimum(xv)))
+        normab(xv,a,b) = norm01(xv) * (b-a) .+ a
+        ## initial and final value positions
+        ivpr = findfirst(!ismissing, res)
+        fvpr = findlast(!ismissing, res)
+
+        x1 = collect(normab(ivpr:fvpr,1,N))
+        y1 = collect(skipmissing(res))
+        yx = zeros(N)
+        
+        # linear estimation of the y axis
+        for i in eachindex(x1[1:(end-1)])
+            xi = x1[i];    yi = y1[i]
+            xi1 = x1[i+1]; yi1 = y1[i+1]
+            for xv in ceil(xi):1:floor(xi1)
+                yx[Int(xv)] = (yi1-yi)*(xv-xi)/(xi1-xi) + yi
+            end
+        end
+        res = yx
     end
 
+    if norm
+        
+    end
+    
     res
+
 end
-
-
