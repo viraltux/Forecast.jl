a03cc387cac4bb871887e019141e8eb96d1f1e42
Fran Urbano <viraltux@gmail.com>
2020-12-15 22:22:01 +0100

    Changing output format and minor changes.

--- src/stl.jl
+++ src/stl.jl
@@ -6,15 +6,9 @@ include("sma.jl")
 include("loess.jl")
 
 """
-    sodd(x)
+        sodd(x)
 
-Return the smallest odd integer greater than or equal to `x`.
-    
-# Examples
-```julia-repl
-julia> sood(3.6)
-5
-```
+    Return the smallest odd integer greater than or equal to `x`.        
 """
 function sodd(x::Number)::Integer
     cx = Integer(ceil(x))
@@ -22,35 +16,41 @@ function sodd(x::Number)::Integer
 end
 
 """
-    stl(Yv, np; robust=false, nl=sodd(np), ns=10*length(Yv)+1,
-                nt=sodd(1.5*np/(1-1.5/ns)), 
-                ni=robust ? 1 : 2,
-                no=robust ? 15 : 0,
-                spm=false)
-    
+        stl(Yv, np; robust=false, nl=sodd(np), ns=10*length(Yv)+1,
+                    nt=sodd(1.5*np/(1-1.5/ns)), 
+                    ni=robust ? 1 : 2,
+                    no=robust ? 15 : 0,
+                    spm=false)
+        
 Decompose a time series into trend, seasonal, and remainder components.
 
-"STL has s simple desing that consists of a sequence of applications of the loess smoother; the simplicity allows analysis of the properties of the procedure and allows fast computation, even for very long time series and large amounts of trend and seasonal smoothing. Other features of STL  are specification of amounts of seasonal and trend smoothing that range, in a nearly continous way, from very small amount of smoothing to a very large amount; roubst estimatews of the trend and seasonal components that are not distorted by aberrant behavior in the data; specification of the period of the seasonal component to any intenger multiple of the time sampling interval greater than one; and the ability to decompose time series with missing values."
+    "STL has s simple desing that consists of a sequence of applications of the loess smoother; the simplicity allows analysis of the properties of the procedure and allows fast computation, even for very long time series and large amounts of trend and seasonal smoothing. Other features of STL  are specification of amounts of seasonal and trend smoothing that range, in a nearly continous way, from very small amount of smoothing to a very large amount; roubst estimatews of the trend and seasonal components that are not distorted by aberrant behavior in the data; specification of the period of the seasonal component to any intenger multiple of the time sampling interval greater than one; and the ability to decompose time series with missing values."
 
-Excerpt from "STL: A Seasonal, Trend Decomposition Procedure Based on Loess
-Robert B. Cleveland, William S. Cleveland, Jean E. McRae, and Irma Terpenning" published in the Journal of Official Statistics Vol. 6. No. 1, 1990, pp. 3-73 (c) Statistics Sweden.
+Excerpt from "STL: A Seasonal, Trend Decomposition Procedure Based on Loess"
+              Robert B. Cleveland, William S. Cleveland, Jean E. McRae, and Irma Terpenning.
+              Journal of Official Statistics Vol. 6. No. 1, 1990, pp. 3-73 (c) Statistics Sweden.
 
-Args:
-    `np`: Sesonality.
-    `robust`: Robust stl.
-    `nl`: Smoothing parameter of the low-pass filter.
-    `ns`: Smoothing parameter for the seasonal component. It is chosen of the basis of knowledge of the time series and on the basis of diagnostic methods; must always be odd and at least 7. The default value is not advised on the original paper but it is  the same chosen by the stl implementation in R.
-    `nt`: Smoothing parameter for the trend decomposition.
-    `ni`: Number of inner loop cycles.
-    `no`: Number of outer loop cycles. The paper advises 5 ("safe value") or 10 ("near certainty of convergence") cycles when robustness is required, however the default is set at 15 following R implementation.
-    `spm`: Seasonal post-smoothing.
+    Args:
+        `np`: Sesonality.
+        `robust`: Robust stl.
+        `nl`: Smoothing parameter of the low-pass filter.
+        `ns`: Smoothing parameter for the seasonal component. It is chosen of the basis of knowledge of the time series and on the basis of diagnostic methods; must always be odd and at least 7. The default value is not advised on the original paper but it is  the same chosen by the stl implementation in R.
+        `nt`: Smoothing parameter for the trend decomposition.
+        `ni`: Number of inner loop cycles.
+        `no`: Number of outer loop cycles. The paper advises 5 ("safe value") or 10 ("near certainty of convergence") cycles when robustness is required, however the default is set at 15 following R implementation.
+        `spm`: Seasonal post-smoothing.
 
-Returns:
-    An `stl` object.
+    Returns:
+        An `stl` object with the seasonal, trend and remainder components.
+
+# Examples
+```julia-repl
+julia> stl(log(AirPassengers),12)
+
+```
 """
 
-function stl(Yv::,
-             np;
+function stl(Yv,np;
              robust=false,
              nl=sodd(np),
              ns=10*length(Yv)+1,
@@ -61,6 +61,7 @@ function stl(Yv::,
     
     @assert mod(ns,2)==1 & (ns>=7) "ns is chosen of the basis of knowledge of the time series and on the basis of diagnostic methods; must always be odd and at least 7"
 
+    #TODO implement the handling of TimeSeries 
     #TODO implement seasonal post-smoothing option?
     #TODO implement multiplicative decomposition Fv?
     #TODO implement convergence criteria? at least return its value
@@ -118,5 +119,7 @@ function stl(Yv::,
         rhov = B.(abs.(Rv)/h)
     end
 
-    (Yv,Tv,Sv,Rv)
+    hcat(Sv,Tv,Rv)
+end
+
 end
