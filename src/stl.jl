c333b9f1a9bbf5060e6e6e10c5b8d9d327d70d5c
Fran Urbano <viraltux@gmail.com>
2020-12-15 21:59:55 +0100

    Create sodd function outside main stl function and draft for initial documentation

--- src/stl.jl
+++ src/stl.jl
@@ -1,44 +1,71 @@
+module Forecast
+
+export stl
+
 include("sma.jl")
 include("loess.jl")
 
-function stl(Yv,np;
-             robust=false,
-             spm=false, #seasonal post-smoothing
-             nl=(mod(ceil(np),2)==0) ? Integer(ceil(np)+1) : Integer(ceil(np)),
-             ns=10*length(Yv)+1, 
-             nt=(mod(ceil(1.5*np/(1-1.5/ns)),2)==0) ? Integer(ceil(1.5*np/(1-1.5/ns))+1) : Integer(ceil(1.5*np/(1-1.5/ns))),
-             ni=robust ? 1 : 2,
-             no=robust ? 15 : 0)
+"""
+    sodd(x)
 
-    # parameters
-    # np #sesonality
-    # no #outer loop cycles
-    # ni #inner loop cycles
-    # nl #smoothing parameter of the low-pass filter
-    # nt #smoothing parameter for the trend decomposition
-    # ns #smoothing parameter for the seasonal component
-    # ns must be carefully tailored for each application
+Return the smallest odd integer greater than or equal to `x`.
     
-    #nodd(1.5*np/(1-1.5/ns)),
-    function nodd(x)
-        cx = ceil(x)
-        (mod(cx,2)==0) ? cx+1 : cx
-    end
+# Examples
+```julia-repl
+julia> sood(3.6)
+5
+```
+"""
+function sodd(x::Number)::Integer
+    cx = Integer(ceil(x))
+    mod(cx,2)==0 ? cx+1 : cx
+end
+
+"""
+    stl(Yv, np; robust=false, nl=sodd(np), ns=10*length(Yv)+1,
+                nt=sodd(1.5*np/(1-1.5/ns)), 
+                ni=robust ? 1 : 2,
+                no=robust ? 15 : 0,
+                spm=false)
+    
+Decompose a time series into trend, seasonal, and remainder components.
+
+"STL has s simple desing that consists of a sequence of applications of the loess smoother; the simplicity allows analysis of the properties of the procedure and allows fast computation, even for very long time series and large amounts of trend and seasonal smoothing. Other features of STL  are specification of amounts of seasonal and trend smoothing that range, in a nearly continous way, from very small amount of smoothing to a very large amount; roubst estimatews of the trend and seasonal components that are not distorted by aberrant behavior in the data; specification of the period of the seasonal component to any intenger multiple of the time sampling interval greater than one; and the ability to decompose time series with missing values."
+
+Excerpt from "STL: A Seasonal, Trend Decomposition Procedure Based on Loess
+Robert B. Cleveland, William S. Cleveland, Jean E. McRae, and Irma Terpenning" published in the Journal of Official Statistics Vol. 6. No. 1, 1990, pp. 3-73 (c) Statistics Sweden.
+
+Args:
+    `np`: Sesonality.
+    `robust`: Robust stl.
+    `nl`: Smoothing parameter of the low-pass filter.
+    `ns`: Smoothing parameter for the seasonal component. It is chosen of the basis of knowledge of the time series and on the basis of diagnostic methods; must always be odd and at least 7. The default value is not advised on the original paper but it is  the same chosen by the stl implementation in R.
+    `nt`: Smoothing parameter for the trend decomposition.
+    `ni`: Number of inner loop cycles.
+    `no`: Number of outer loop cycles. The paper advises 5 ("safe value") or 10 ("near certainty of convergence") cycles when robustness is required, however the default is set at 15 following R implementation.
+    `spm`: Seasonal post-smoothing.
+
+Returns:
+    An `stl` object.
+"""
+
+function stl(Yv::,
+             np;
+             robust=false,
+             nl=sodd(np),
+             ns=10*length(Yv)+1,
+             nt=sodd(1.5*np/(1-1.5/ns)),
+             ni=robust ? 1 : 2,
+             no=robust ? 15 : 0,
+             spm=false)
     
     @assert mod(ns,2)==1 & (ns>=7) "ns is chosen of the basis of knowledge of the time series and on the basis of diagnostic methods; must always be odd and at least 7"
 
-    
-    # if robustness is not needed ni = 2 and no = 0 otherwise
-    # ni = 1 and no = 5 (no = 10 near certainty convergence)
-    # it can be coded a convergence criterion such an in section 3.3
-
-    #TODO implement seasonal post-smoothing option
-    #TODO implement multiplicative decomposition Fv
-    #TODO implement convergence criteria or at least return its value
+    #TODO implement seasonal post-smoothing option?
+    #TODO implement multiplicative decomposition Fv?
+    #TODO implement convergence criteria? at least return its value
     # with a recomendation if not converge takes place
     # max(abs(Sv-Sv1))/(max(Sv)-min(Sv)) < 0.01 then convergence takes place
-
-    #using R defaults for ns and no
     
     function B(u)
         u < 1 ? (1-u^2)^2 : 0
