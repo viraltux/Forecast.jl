e1e5402a204644121cabba77278ac929e7bfd474
Fran Urbano <viraltux@gmail.com>
2020-12-17 22:50:37 +0100

    updating entry for the new loess signature; using rho instead k

--- src/stl.jl
+++ src/stl.jl
@@ -65,16 +65,8 @@ function stl(Yv,np;
     
     @assert mod(ns,2)==1 & (ns>=7) "ns is chosen of the basis of knowledge of the time series and on the basis of diagnostic methods; must always be odd and at least 7"
 
-    #TODO implement the handling of TimeSeries
-    #TODO check on discrepancies with R stl
-    #TODO implement seasonal post-smoothing option?
-    #TODO implement multiplicative decomposition Fv?
-    #TODO implement convergence criteria? at least return its value
-    # with a recomendation if not converge takes place
-    # max(abs(Sv-Sv1))/(max(Sv)-min(Sv)) < 0.01 then convergence takes place
-    
     function B(u)
-        u < 1 ? (1-u^2)^2 : 0
+        u < 1 ? (1-u^2)^2 : 0.0
     end
 
     N = length(Yv)
@@ -84,28 +76,35 @@ function stl(Yv,np;
     Tv = zeros(N)
     Sv = Array{Float64,1}(undef,N)
     Rv = Array{Float64,1}(undef,N)
-    for o in 1:(no+1)
+    Cv = Array{Float64,1}(undef, N+2*np)
+    cnv = false # Seasonality convergence flag
+    for o in 0:no
         for k in 1:ni
             # Updating sesonal and trend components
             ## 1. Detrending (Yv = Tv + Sv)
             Sv = Yv - Tv
-
             # Sesonal Smoothing 2,3,4
             ## 2. Cycle-subseries Smoothing
-            Cv = zeros(N+2*np)
             for csi in 1:np
-                Cv[csi:np:N+2*np] = loess(csi:np:N,Sv[csi:np:N];
-                                          q=ns,d=1,k=rhov[csi:np:N],
+                Cv[csi:np:N+2*np] = loess(csi:np:N,
+                                          Sv[csi:np:N];
+                                          q=ns,d=1,rho=rhov[csi:np:N],
                                           predict=(1.0*csi-np):np:(N+np))
             end
             ## 3. Low-Pass Filtering of Smoothed Cycle-Subseries
             ### using -div(N,2)+1.:div(N,2) instead 1.:N to balance out machine error (center-right)
-            Lv = loess(-div(N,2)+1.:div(N,2),collect(skipmissing(sma(sma(sma(Cv,np),np),3))),d=1,q=nl)
+            Lv = loess(-div(N,2)+1.:div(N,2),
+                       collect(skipmissing(sma(sma(sma(Cv,np),np),3))),
+                       d=1,q=nl,rho=rhov)
             ## 4. Detreending of Smoothed Cycle-Subseries
 
             ### Lv is substracted to prevent low-frenquency power
             ### from entering the seasonal component.
-            Sv = Cv[np+1:end-np] - Lv
+            Sv1 = Cv[np+1:end-np] - Lv
+
+            ### Seasonal convergence criterion
+            cnv = (maximum(abs.(Sv1-Sv))/(maximum(Sv)-minimum(Sv)) < 0.01)
+            Sv = Sv1
             
             ## 5. Deseasonalizing
             Dv = Yv - Sv
@@ -113,7 +112,7 @@ function stl(Yv,np;
             # Trend Smoothing
             ## 6. Trend Smoothing
             ### using -div(N,2):div(N,2)-1. instead 1.:N to balance out machine error (center-left)
-            Tv = loess(-div(N,2):div(N,2)-1.,Dv,q=nt,d=1,k=rhov)
+            Tv = loess(-div(N,2):div(N,2)-1.,Dv,q=nt,d=1,rho=rhov)
         end
         # Computation of robustness weights
         ## These new weights will reduce the influence of transient behaviour
@@ -122,8 +121,17 @@ function stl(Yv,np;
         # Tv and Sv defined everywhere but Rv is not defined
         # where Yv has missing values
         Rv = Yv - Tv - Sv
-        h = 6*median(abs.(Rv))
-        rhov = B.(abs.(Rv)/h)
+        if 0 < o <= no
+            smRv = skipmissing(Rv)
+            h = 6*median(abs.(smRv))
+            rhov = B.(abs.(smRv)/h)
+        end
+    end
+
+    if cnv
+        @info "Seasonal corvengence achieved"
+    else
+        @warn "Seasonal convergence not achieved, consider increasing the number of inner or/and outer cycles"
     end
 
     hcat(Sv,Tv,Rv)
