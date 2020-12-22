1f346c824596341eb8904e41e81edd9f2e3d5792
Fran Urbano <viraltux@gmail.com>
2020-12-22 16:11:52 +0100

    documenting loess

--- src/loess.jl
+++ src/loess.jl
@@ -1,11 +1,5 @@
-## Loess
-using LinearAlgebra
-using Statistics
-
-
 # TODO Check faster implementations in computational methods for local regression
 # William S. Cleveland & E. Grosse https://link.springer.com/article/10.1007/BF01890836
-
 function ghat(x::Float64;A,b,d=2,q,rho)
               
     xv = A[:,d]
@@ -33,11 +27,50 @@ function ghat(x::Float64;A,b,d=2,q,rho)
 
 end
 
+
+"""
 function loess(xv,yv;
                d=2,
                q=Int64(round(3/4*length(xv))),
                rho=repeat([1.0],inner=length(xv)),  
-               iter = 3,
+               predict = xv)
+
+Smooth a vector of observations using locally weighted regressions.
+
+Although loess can be used to smooth observations for any given number of independent variables, this implementation is univariate. The speed of loess can be greatly increased by using fast aproximations for the linear fitting calculations, however this implementation calculates only exact results.
+
+The loess functionality and nomenclature follows the descriptions in:
+
+    "STL: A Seasonal, Trend Decomposition Procedure Based on Loess"
+     Robert B. Cleveland, William S. Cleveland, Jean E. McRae, and Irma Terpenning.
+     Journal of Official Statistics Vol. 6. No. 1, 1990, pp. 3-73 (c) Statistics Sweden.
+
+    Args:
+        `xv`: Observations' support.
+        `yv`: Observation values.
+        `d`: Degree of the linear fit, it accepts values 1 or 2.
+        `q`: As q increases loess becomes smoother, when q tends to infinity loess tends to an ordinary least square poynomial fit of degree `d`. It defaults to the rounding of 3/4 of xv's length.
+        `rho`: Weights expressing the reliability of the observations (e.g. if yi had variances sigma^2*ki where ki where known, the rhoi could be 1/ki). It defaults to 1.0.
+        `predict`: Vector containing the real values to be predicted, by default predicts xv.
+    Returns:
+        The loess values for the values contained in `predict`.
+
+# Examples
+```julia-repl
+julia> loess(rand(5),rand(5))
+5-element Array{Float64,1}:
+ 0.8279856470091445
+ 0.6498793177642597
+ 0.017078421335739336
+ 0.8595786774014016
+ 0.2564676547690037
+```
+"""
+
+function loess(xv,yv;
+               d=2,
+               q=Int64(round(3/4*length(xv))),
+               rho=repeat([1.0],inner=length(xv)),  
                predict = xv)
     
     @assert (d==1) | (d==2) "Linear Regression must be of degree 1 or 2"
@@ -48,7 +81,6 @@ function loess(xv,yv;
     yv = yv[myi]
     rho = rho[myi]
     
-    #ghat.(xv;xv=xv,yv=yv,q=q,d=d)
     res = zeros(length(predict))
 
     ## Ax = b
@@ -57,11 +89,6 @@ function loess(xv,yv;
     d == 2 ? A = hcat(xv .^ 2.0, A) : nothing
 
     for (i,xi) in enumerate(predict)
-        # TODO find out why expanding this ghat function in this loops causes a
-        # drop in performance. For 10,000 values it goes from
-        #   6.595928 seconds (210.07 k allocations: 6.534 GiB, 4.96% gc time)
-        # to
-        #   26.257998 seconds (890.11 M allocations: 20.167 GiB, 7.03% gc time)
         res[i] = ghat(xi;A=A,b=b,d=d,q=q,rho=rho)
     end
     res
