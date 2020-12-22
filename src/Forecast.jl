74802699f5acbb782907f8d41911e02f7aef9b1e
Fran Urbano <viraltux@gmail.com>
2020-12-22 17:53:25 +0100

    first draft

--- src/Forecast.jl
+++ src/Forecast.jl
@@ -1,5 +1,26 @@
 module Forecast
 
-# Write your package code here.
+using LinearAlgebra, TimeSeries
+
+# types
+export STL
+# methods
+export loess, sma, stl
+
+# source files
+include("utils.jl")
+include("sma.jl")
+include("loess.jl")
+
+"""
+Collection of methods for Time Series analysis
+
+Methods implemented:
+
+    loess:      Locally weighted smoothed series.
+    sma:        Simple moving average.
+    stl:        Seasonal and Trend decomposition using loess.
+"""
+Forecast
 
 end
