5c819f92ef2251b7f45e4ea3ee536fd3d63e78cb
Fran Urbano <viraltux@gmail.com>
2020-12-22 18:02:46 +0100

    utility functions

---
+++ src/utils.jl
@@ -0,0 +1,9 @@
+"""
+    nextodd(x)
+
+Return the smallest odd integer greater than or equal to `x`.        
+"""
+function nextodd(x::Real)::Integer
+    cx = Integer(ceil(x))
+    mod(cx,2)==0 ? cx+1 : cx
+end
