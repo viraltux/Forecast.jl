b4fe9707bd536afd8728afcc22d79373191f0892
Fran Urbano <viraltux@gmail.com>
2020-12-24 00:05:46 +0100

    initial basic tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,37 @@
-using Forecast
 using Test
+using Forecast
+using Random
+using Logging
 
-@testset "Forecast.jl" begin
-    # Write your tests here.
+const tests = [
+    "sma",
+    "loess",
+    "stl"
+]
+
+printstyled("\nTest Summary List:\n", color=:underline)
+
+Random.seed!(36)
+
+# include with hidden stderr
+function include_hide(file)
+    io = open("/dev/null", "w")
+    redirect_stderr(io) do
+        logger = ConsoleLogger(stderr)
+        with_logger(logger) do
+            include(file)
+        end
+    end
 end
+
+for t in tests
+    @testset "Test $t" begin
+        Random.seed!(36)
+        include_hide("$t.jl")
+        println()
+    end
+end
+
+println("\nAmbiguous methods: ")
+display(detect_ambiguities(Forecast, imported=true))
+println()
