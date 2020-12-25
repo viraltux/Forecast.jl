7d30ea110b4195fcf0f2837c176bfc5d103d1053
Fran Urbano <viraltux@gmail.com>
2020-12-25 13:49:57 +0100

    import DataFrames: DataFrame

--- test/datasets.jl
+++ test/datasets.jl
@@ -1,11 +1,12 @@
 using Test
 using Forecast
+import DataFrames: DataFrame
 
 @testset "co2" begin
     co2_ta = co2()
     co2_df = co2(true)
-    @test co2_ta isa TimeSeries.TimeArray
-    @test co2_df isa DataFrames.DataFrame
+    @test co2_ta isa TimeArray
+    @test co2_df isa DataFrame
     @test size(co2_ta) == (4609,)
     @test size(co2_df) == (17166, 17)
 end
