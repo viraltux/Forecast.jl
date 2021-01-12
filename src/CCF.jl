fed7a7ba46725bcfaa818094965569cbdfec9812
Fran Urbano <viraltux@gmail.com>
2021-01-12 08:42:51 +0100

    update CCF

--- src/CCF.jl
+++ src/CCF.jl
@@ -5,22 +5,22 @@ Store results from the functions acf, ccf and pacf
 
 # Arguments
 
-    `ccf::T`          An array with results from ccf, acf and pacf
-    `N::Integer`      Length of ccf
-    `type::String`    Type of CCF
-    `lag::Integer`    Maximum number of lags
-    `alpha::Tuple`    CI thresholds
-    `ci::Tuple`       CI for alpha
-    `auto::Bool`      Auto-correlation
-    `call::String`    Method called to generate ccf
+    `ccf::AbstractArray`    An array with results from ccf, acf and pacf
+    `N::Integer`            Length of ccf
+    `type::String`          Type of CCF
+    `lag::Integer`          Maximum number of lags
+    `alpha::Tuple`          CI thresholds
+    `ci::Tuple`             CI for alpha
+    `auto::Bool`            Auto-correlation
+    `call::String`          Method called to generate ccf
 """
-mutable struct CCF{T<:AbstractArray}
-    ccf::T             # An array with results from ccf, acf and pacf
-    N::Integer         # Length of ccf
-    type::String       # Type of CCF
-    lag::Integer       # Maximum number of lags
-    alpha::Tuple       # CI thresholds
-    ci::Tuple          # CI for alpha
-    auto::Bool         # Auto-correlation
-    call::String       # Method called to generate ccf
+mutable struct CCF
+    ccf::AbstractArray 
+    N::Integer         
+    type::String       
+    lag::Integer       
+    alpha::Tuple{Float64,Float64}
+    ci::Tuple{Float64,Float64}
+    auto::Bool         
+    call::String       
 end
