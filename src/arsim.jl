bc706fc5c4d7d83e71ce188c7a1b997c3bf3b97e
Fran Urbano <viraltux@gmail.com>
2021-04-21 22:17:26 +0200

    refactor arsim and expand test

--- src/arsim.jl
+++ src/arsim.jl
@@ -6,7 +6,7 @@ Package: Forecast
     arsim(Φ,Φ0,x0,n)
     arsim(Φ,Φ0,x0,Σ,n)
     arsim(Φ,Φ0,x0,E,n)
-    arsim(AR,n::Integer)
+    arsim(AR,n)
 
 Simulate a multivariate autoregressive series model.
 
@@ -22,74 +22,84 @@ Xt = \\Phi_0 + \\sum_{i=1}^p \\Phi_i \\cdot X_{t-i} + E
 `x0`           Array with dimensions (m,p) for the initial value in the AR model. Default value is a random value from zero to one.
 `Σ`            Variance/Covariance matrix for the AR model with a MvNormal distribution for the noise. Default value is an identity Matrix.
 `n`            Number of simulations.
-x`E`            Distribution for the error.
+`E`            Distribution for the error.
 `AR`           AR struct coming from an `ar` fitting.
 
 # Returns
 A multivariate series simulating an AR model each column containing a dimension and ordered by time ascending rows.
 """
 function arsim(Φ,n::Integer)
-    Φ = Φ isa Number ? [Φ] : Φ
-    sΦ = size(Φ)
-    m = Φ isa Vector ? 1 : sΦ[1]
-    p = length(sΦ) > 2 ? sΦ[3] : 1
-    p = Φ isa Vector ? sΦ[1] : p
+    arsim(Φ,nothing,nothing,MvNormal(0,0),n)
+end
 
-    Φ0 = zeros(m)
-    x0 = rand(m,p)
-    arsim(Φ,Φ0,x0,MvNormal(m,1),n::Integer)
+function arsim(Φ,Φ0,n::Integer)
+    arsim(Φ,Φ0,nothing,MvNormal(0,0),n)
+end
+
+function arsim(Φ,Φ0,x0,n::Integer)
+    arsim(Φ,Φ0,x0,MvNormal(0,0),n)
+end
+
+function arsim(Φ,Φ0,x0,Σ::Number,n::Integer)
+    arsim(Φ,Φ0,x0,MvNormal(1,Σ),n)
+end
+
+function arsim(Φ,Φ0,x0,Σ::Vector,n::Integer)
+    arsim(Φ,Φ0,x0,MvNormal(collect(Diagonal(Σ))),n)
+end
+
+function arsim(Φ,Φ0,x0,Σ::Matrix,n::Integer)
+    arsim(Φ,Φ0,x0,MvNormal(Σ),n)
 end
 
 function arsim(ar_str::AR,n::Integer)
     Φ = ar_str.Φ
-    sΦ = size(Φ)
-    m = sΦ[1]
-    p = length(sΦ) > 2 ? sΦ[3] : 1
-
     Φ0 = ar_str.Φ0
     x0 = ar_str.x0
     Σ = ar_str.Σ
-    arsim(Φ,Φ0,x0,Σ,n::Integer)
-end
-
-function arsim(Φ,Φ0,n::Integer)
-    sΦ = size(Φ)
-    m = sΦ[1]
-    p = length(sΦ) > 2 ? sΦ[3] : 1
-
-    x0 = rand(m,p)
-    arsim(Φ,Φ0,x0,MvNormal(m,1),n::Integer)
-end
-
-function arsim(Φ,Φ0,x0,n::Integer)
-    arsim(Φ,Φ0,x0,MvNormal(size(Φ)[1],1),n::Integer)
-end
-
-function arsim(Φ,Φ0,x0,Σ::AbstractArray,n::Integer)
-    arsim(Φ,Φ0,x0,MvNormal(Σ),n::Integer)
+    arsim(Φ,Φ0,x0,Σ,n)
 end
 
 function arsim(Φ,Φ0,x0,E::Distribution,n::Integer)
+    Φ = compact(Φ)
 
-    Φ = Φ isa Number ? [Φ] : Φ
+    d = ndims(Φ)
     sΦ = size(Φ)
-    m = Φ isa Vector ? 1 : sΦ[1]
-    p = length(sΦ) > 2 ? sΦ[3] : 1
-    p = Φ isa Vector ? sΦ[1] : p
+    if d == 0
+        m,p = 1,1
+    elseif d == 1
+        m,p = 1,sΦ[1]
+    elseif d == 2
+        m,p = sΦ[1],1
+    elseif d == 3
+        m,p = sΦ[1],sΦ[3]
+    else
+        @error "Φ should have less the 4 dimensions"
+    end
 
-    # @assert m == size(Φ0)[1]
-    # @assert size(x0) = (p,m)
-
-    e = rand(E,n::Integer)
-    x = Array{Float64}(undef,n,m)
-
-    # reshape for matrix operation
-    Φ = reshape(Φ,m,m*p) #TODO check size Φ if just two dimensions not reshape
-    x0 = reshape(x0,m*p,1)
+    Φ0 = isnothing(Φ0) ? compact(zeros(m)) : compact(Φ0)
+    x0 = isnothing(x0) ? compact(rand(m,p)) : compact(x0)
+    E = size(E)[1] == 0 ? MvNormal(m,1) : E
     
+    e = rand(E,n)
+    x = compact(Array{Float64}(undef,n,m))
+    
+    # reshape and format for matrix operation
+    Φ = d == 0 ?  reshape([Φ],1,1) : reshape(Φ,m,m*p)
+    Φ0 = d <= 1 ? [Φ0] : Φ0
+    x0 = d == 0 ? [x0] : reshape(x0,m*p,1)
+
     for i in 1:n
-        x[i,:] =  Φ * x0 + Φ0  + e[:,i]
+        x[i,:] = Φ * x0 + Φ0  + e[:,i]
         x0 = vcat(x[i,:],x0)[1:m*p]
     end
-    x
+
+    compact(x)
+end
+
+## Standarize format for calculations
+function compact(x)
+    if x isa Number return x end
+    x = dropdims(x, dims = tuple(findall(size(x) .== 1)...))
+    ndims(x) == 0 ? x[1] : x
 end
--- test/arsim.jl
+++ test/arsim.jl
@@ -2,6 +2,95 @@ using Test
 using Forecast
 
 @testset "arsim" begin
-    x = arsim(1,10)
-    @test x isa AbstractArray
+
+    # arsim(Φ,n)
+    Φ,n = .1,10
+    x = arsim(Φ,n)
+    @test x isa Vector
+    @test length(x) == 10
+
+    Φ,n = [.1,.2],10
+    x = arsim(Φ,n)
+    @test x isa Vector
+    @test length(x) == 10
+
+    Φ,n = [.1 .2; .3 .4],10
+    x = arsim(Φ,n)
+    @test x isa Matrix
+    @test size(x) == (10,2)
+
+    # arsim(Φ,Φ0,n)
+    Φ,Φ0,n = .1,.2,10
+    x = arsim(Φ,Φ0,n)
+    @test x isa Vector
+    @test length(x) == 10
+
+    Φ,Φ0,n = [.1,.2],.3,10
+    x = arsim(Φ,Φ0,n)
+    @test x isa Vector
+    @test length(x) == 10
+
+    Φ,Φ0,n = [.1 .2; .3 .4],[.5, .6],10
+    x = arsim(Φ,Φ0,n)
+    @test x isa Matrix
+    @test size(x) == (10,2)
+
+    # arsim(Φ,Φ0,x0,n)
+    Φ,Φ0,x0,n = .1,.2,.3,10
+    x = arsim(Φ,Φ0,x0,n)
+    @test x isa Vector
+    @test length(x) == 10
+
+    Φ,Φ0,x0,n = [.1,.2],.3,[.4,.5],10
+    x = arsim(Φ,Φ0,x0,n)
+    @test x isa Vector
+    @test length(x) == 10
+
+    Φ,Φ0,x0,n = [.1 .2; .3 .4],[.5, .6],[.7,.8],10
+    x = arsim(Φ,Φ0,x0,n)
+    @test x isa Matrix
+    @test size(x) == (10,2)
+
+    # arsim(Φ,Φ0,x0,Σ,100)
+    Φ,Φ0,x0,Σ,n = .1,.2,.3,.4,10
+    x = arsim(Φ,Φ0,x0,Σ,n)
+    @test x isa Vector
+    @test length(x) == 10
+
+    Φ,Φ0,x0,Σ,n = [.1,.2],.3,[.4,.5],.6,10
+    x = arsim(Φ,Φ0,x0,Σ,n)
+    @test x isa Vector
+    @test length(x) == 10
+
+    Φ,Φ0,x0,Σ,n = [.1 .2; .3 .4],[.5, .6],[.7,.8],[.9 .10; .10 .9],10
+    x = arsim(Φ,Φ0,x0,Σ,n)
+    @test x isa Matrix
+    @test size(x) == (10,2)
+
+    
+    Φ,Φ0,x0,Σ,n = reshape([[.1 .2; .3 .4] [.5 .6; .7 .8]],2,2,2),
+                  [.9, .10],[.11 .12; .13 .14],[.9 .10; .10 .9],10
+    x = arsim(Φ,Φ0,x0,Σ,n)
+    @test x isa Matrix
+    @test size(x) == (10,2)
+
+    
+    # arsim(Φ,Φ0,x0,E,100)
+    E1 = MvLogNormal(MvNormal(1,1))
+    x = arsim(.1,.2,.3,E1,10)
+    @test x isa Vector
+    @test length(x) == 10
+
+    x = arsim([.1,.2],.3,[.4,.5],E1,10)
+    @test x isa Vector
+    @test length(x) == 10
+
+    E2 = MvLogNormal(MvNormal(2,1))
+    x = arsim([.1 .2; .3 .4],[.5, .6],[.7,.8],E2,10)
+    @test x isa Matrix
+    @test size(x) == (10,2)
+
+    # arsim(AR,100)
+    # ar_str = ar
+    
 end
