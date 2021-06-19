2d92587069f13a4bab6b0d6776ac9395373b49c7
Fran Urbano <viraltux@gmail.com>
2021-06-19 19:16:48 +0200

    refactor sma

--- test/pacf.jl
+++ test/pacf.jl
@@ -3,9 +3,9 @@ using Forecast
 
 @testset "pacf" begin
     x = rand(100);
-    @test_throws AssertionError pacf(x; lag = 100)
+    @test_throws AssertionError Forecast.pacf(x; lag = 100)
 
-    res = pacf(x; lag = 20);
+    res = Forecast.pacf(x; lag = 20);
     @test res isa CCF
     @test res.call == "pacf(x; type=\"stepwise-real\", lag=20, alpha=(0.95, 0.99))"
     @test res.type == "pacf_stepwise-real"
