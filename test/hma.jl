5b951cdfadfddf09c49826a9b9e943ecf2447841
Fran Urbano <viraltux@gmail.com>
2021-06-24 10:37:16 +0200

    Update hma.jl

--- test/hma.jl
+++ test/hma.jl
@@ -1,13 +1,41 @@
+# MIT License
+
+# Copyright (c) 2021 Fran Urbano
+# Copyright (c) 2021 Val Lyashov
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 using Test
 using Forecast
 import Forecast: hmaSymmetricWeights
 
 @testset "hma" begin
 
-    # weights (https://www.mathworks.com/help/econ/seasonal-adjustment-using-snxd7m-seasonal-filters.html)
-    x = hmaSymmetricWeights(13)
-    @test length(x) == 13
-    @test x         == [-0.019, -0.028, 0.0, 0.065, 0.147, 0.214, 0.24, 0.214, 0.147, 0.065, 0.0, -0.028, -0.019]  
+    # weights 
+    w = hmaSymmetricWeights(7)
+    @test sum(w) ≈ 1.0
+    w = hmaSymmetricWeights(13)
+    @test sum(w) ≈ 1.0
+    # (https://www.mathworks.com/help/econ/seasonal-adjustment-using-snxd7m-seasonal-filters.html)
+    @test length(w) == 13
+    @test w         ≈ [-0.019, -0.028, 0.0, 0.065, 0.147, 0.214, 0.24,
+                       0.214, 0.147, 0.065, 0.0, -0.028, -0.019]  
 
     # 13-term moving average
     x = hma(sin.(1:100), 13)
@@ -15,5 +43,6 @@ import Forecast: hmaSymmetricWeights
     @test first(x)  ≈ 0.5634778709798388
     @test last(x)   ≈ -0.664567410322129
     @test x[50]     ≈ -0.044034801763622136
+
 
 end
